57e5c19ec853238e6fddb9179b69b0cedc2c029d
Sangjin <hn04147@naver.com>
2023-06-08 00:54:24 +0900

    sample deploy

---
+++ components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = ({ title }) => {
+    return (
+        <div>
+            <div className='pt-10' />
+            <div className='text-3xl font-bold text-sky-700'>{title}</div>
+            <div className='h-1 w-20 my-5 bg-sky-700' />
+        </div>
+    )
+}
+
+export default Title
---
+++ pages/Education.jsx
@@ -0,0 +1,22 @@
+import Title from "../components/Title"
+
+const Education = () => {
+    return (
+        <div className='w-full p-2'>
+            <Title title={'Education'} />
+            <div className='w-full h-16 flex'>
+                <div className='w-48 lg:w-64 h-full grid content-center'>
+                    <div className='text-sm text-slate-500'>2016.03 - 2023.08</div>
+                    <div className='font-bold'>한양대학교</div>
+                </div>
+                <div className='h-full grid content-center'>
+                    <div className=''>컴퓨터소프트웨어학부</div>
+                    <div className=''>3.39 / 4.5</div>
+                </div>
+            </div>
+            <div className='h-[1px] w-full my-4 bg-stone-200' />
+        </div>
+    )
+}
+
+export default Education
